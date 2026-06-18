fbc8672684c18c31a08205c6027b95a157b46261
naveen_v <vnaveen86080@gmail.com>
2026-06-18 23:14:44 +0530

    add logic initiate

---
+++ Calculator/main.js
@@ -0,0 +1,90 @@
+let calcNums=document.querySelectorAll(".numbers")
+let calcSym=document.querySelectorAll(".symbol")
+let calcBox=document.querySelector("input")
+
+let clear=document.querySelector("#clear")
+let equal=document.getElementById("equal")
+
+calcNums.forEach((element) => {
+    element.addEventListener("click",(e)=>{
+      calcBox.value
+      +=e.target.textContent
+
+    })
+    
+    
+});
+
+calcSym.forEach((sym)=>{
+    sym.addEventListener("click",(s)=>{
+        calcBox.value+=s.target.textContent
+    })
+})
+
+
+
+
+
+
+ let numbers=[]
+    let operators=[]
+
+    function finalOut(){
+        let n=calcBox.value
+        let current=""
+        for (i=0;i<n.length;i++){
+    
+         if(n[i]=="+"||n[i]=="-"){
+            operators.push(n[i])
+            numbers.push(current)
+            current=""
+        }
+        
+        else{
+        current+=n[i]
+        
+        }
+        }
+        numbers.push(current)
+        calculate()
+ 
+}
+
+let sum;
+function calculate(){
+    
+    
+    sum=Number(numbers[0])
+    for(x=0;x<operators.length;x++){
+    if(operators[x]=="+"){
+    sum+=Number(numbers[x+1])
+    
+    }
+    
+    
+}
+console.log(sum)
+
+}
+
+
+   function del(){
+    calcBox.value=""
+    numbers.splice(0,numbers.length)
+    operators.splice(0,operators.length)
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
